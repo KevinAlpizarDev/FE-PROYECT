fb0d62f494847dbd34b6b72c4a0dab46823e26d7
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-07-11 17:41:50 -0600

    input and button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -58,7 +58,7 @@ const Contact = () => {
         <textarea  class="bg-pink-100 text-white w-72 c h-14 rounded-xl block flex-1 bg-transparent py-4 pl-4  placeholder:text-gray-400 focus:none sm:text-md sm:leading-6 text-lg" name="message" placeholder="Message to MicroWorld" />
       </div>
         <input type="email" name="user_email"  id="email" autocomplete="email" value="" class="border w-72 h-14 rounded-xl block flex-1 bg-transparent py-4 pl-4 text-gray-900 placeholder:text-gray-400 focus:none sm:text-md sm:leading-6 text-lg" placeholder="Enter your e-mail"></input>
-        <input  class="bg mx-auto bg-blue-700 text-white px-6 py-2 rounded-xl shadow border font-medium mt-4 text-xl hover:brightness-90" type="submit" value="Message" />
+        <input  class="bg  bg-blue-700 text-white px-6 py-2 rounded-xl shadow border font-medium mt-4 text-xl hover:brightness-90 mx-3" type="submit" value="Message" />
       </div>
     </div>
   </div>
